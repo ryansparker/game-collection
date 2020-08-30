4989411393d9897c392154dedae49c3b064588f6
Ryan Parker <ryansparker@gmail.com>
2020-08-30 11:16:37 -0400

    updating homepage with footer and video

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 //Import Components
-import GamesMenu from './components/GamesMenu'
+
 import Home from './components/Home'
+
 
 function App() {
   
   return (
     <div className="App">
       <Home />
-      <GamesMenu />
+     
     </div>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import './index.css';
-
+import GamesMenu from '../GamesMenu'
+import Footer from '../Footer';
+import Waves from '../Video/waves.mp4'
 
 function Home() {
 
 
 return (
     <div id="home-box">
+        <video className="video" autoPlay loop muted>
+            <source src={Waves} type="video/mp4"/>
+        </video>
+
+        <div className="home-about">
+        <h3 className="about">About</h3>
+        </div>
+
         <div className="home-title">
         <h1>Games Collection</h1>
         </div>
-        
+        <GamesMenu />
+      <Footer />
+
     </div>
 )
 }
