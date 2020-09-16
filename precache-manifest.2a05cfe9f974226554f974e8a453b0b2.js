da1dd89b2757a311caf7860451484643abde4805
Ryan Parker <ryansparker@gmail.com>
2020-09-16 13:32:23 -0400

    Updates

---
+++ precache-manifest.2a05cfe9f974226554f974e8a453b0b2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b749869de483d5427d06b089fe87528",
+    "url": "/game-collection/index.html"
+  },
+  {
+    "revision": "68094673ae18e23f9da6",
+    "url": "/game-collection/static/css/main.fbf0d6de.chunk.css"
+  },
+  {
+    "revision": "062c5f3d42dccfb2eb07",
+    "url": "/game-collection/static/js/2.eb238962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-collection/static/js/2.eb238962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68094673ae18e23f9da6",
+    "url": "/game-collection/static/js/main.2265bf60.chunk.js"
+  },
+  {
+    "revision": "778140b7f7c704680f59",
+    "url": "/game-collection/static/js/runtime-main.be04cb94.js"
+  },
+  {
+    "revision": "f41b5c23956b6526a7b29c2a501e0a87",
+    "url": "/game-collection/static/media/about.f41b5c23.svg"
+  },
+  {
+    "revision": "d88a71c728c2cef478e0c8c64ea9400a",
+    "url": "/game-collection/static/media/clown.d88a71c7.mp4"
+  },
+  {
+    "revision": "a61cab67dd5bbc9064aa708789899257",
+    "url": "/game-collection/static/media/dice.a61cab67.mp4"
+  },
+  {
+    "revision": "aea2adc7ad9fce04634be54cbd160a03",
+    "url": "/game-collection/static/media/game-clown.aea2adc7.svg"
+  },
+  {
+    "revision": "73501eb7b35e50ef6d82499008f9b2fc",
+    "url": "/game-collection/static/media/game-dice.73501eb7.svg"
+  },
+  {
+    "revision": "b335b9cdd2ebf0f442bde771888f209f",
+    "url": "/game-collection/static/media/game-snake.b335b9cd.svg"
+  },
+  {
+    "revision": "992d3c6d572b6b828cd3c231d72c29c4",
+    "url": "/game-collection/static/media/gradient.992d3c6d.mp4"
+  },
+  {
+    "revision": "76d05cba7561dff10ce298da49ea5281",
+    "url": "/game-collection/static/media/snake.76d05cba.mp4"
+  }
+]);
